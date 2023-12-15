9919faa70e4b4114f07b8208b41ad4ee7468e1ea
Hamid Azim <hamidazim321@gmail.com>
2023-12-15 21:31:12 +0500

    Make header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,95 @@
 import React, { useState } from "react";
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
 
 export default function Header() {
-  const [menuOn, setMenuOn] = useState(false)
-  const menuOnStyle = 'h-screen absolute top-0 right-0 z-10'
-  let menuStyle = menuOn ? menuOnStyle : ''
+  const [menuOn, setMenuOn] = useState(false);
 
   const handleMenu = () => {
-    setMenuOn(prev => !prev)
-  }
+    setMenuOn((prev) => !prev);
+  };
+
   return (
-    <div className="border-4 flex flex-row justify-between relative">
-      <div className="px-4">Logo</div>
-      <div className={`sm:static px-4 sm:h-auto bg-slate-400 flex flex-col w-full ${menuStyle}`}>
-        <button className="sm:hidden self-end" onClick={handleMenu}>Hamburger</button>
-        <nav className={`${menuOn ? '' : 'hidden'} flex sm:flex flex-col sm:flex-row items-center sm:gap-4 sm:self-end`}>
-          <a>Action</a>
-          <a>Action</a>
-          <a>Action</a>
-        </nav>
-      </div>
-    </div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 w-full">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 dark:bg-slate-600">
+          <a
+          href="https://www.linkedin.com/in/hamid-azim/"
+          className="select-none"
+          >
+            {/* <img
+            Logo
+          /> */}
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Hamid
+            </span>
+          </a>
+          <DarkThemeToggle className="dark:text-white text-yellow-300 border-2 me-auto ms-4"/>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+            onClick={handleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5 text-black dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className={`${!menuOn && "hidden"} w-full md:block md:w-auto relative`}
+            id="navbar-default"
+          >
+            <ul className={`${menuOn && 'absolute w-full'} md:static font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700`}>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Professional
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
   );
 }
