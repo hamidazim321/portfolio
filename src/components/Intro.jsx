5a288d1caa904b6c38805db4e160fc94139db102
Hamid Azim <hamidazim321@gmail.com>
2023-12-17 18:43:32 +0500

    Add intro component

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Intro({className}) {
+  return (
+    <div className={className} >
+      <p className='font-sans dark:text-white font-semibold text-4xl md:text-5xl'>Intro</p>
+      <p className='dark:text-white font-sans text-lg md:text-2xl sm:text-xl'>
+      I'M A PARAGRAPH. CLICK HERE TO ADD YOUR OWN TEXT AND EDIT ME. IT’S 
+      EASY. JUST CLICK “EDIT TEXT” OR DOUBLE CLICK ME TO ADD YOUR OWN CONTENT 
+      AND MAKE CHANGES TO THE FONT. FEEL FREE TO DRAG AND DROP ME ANYWHERE YOU 
+      LIKE ON YOUR PAGE. 
+      </p>
+      <button 
+      type='button' 
+      className='font-semibold font-sans dark:text-white border-2 border-green-400 hover:bg-green-400 p-2'
+      >
+        Get My Resume
+      </button>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Intro from "../components/Intro";
 
 export default function Home() {
   return (
-    <div className="bg-headshot relative bg-no-repeat bg-cover md:bg-contain md:bg-right">
+    <div className="bg-headshot relative bg-no-repeat bg-cover md:bg-contain md:bg-right bg-fixed">
       <div className="dark:bg-slate-600/75 h-full relative z-10 w-full ps-8 sm:ps-16 pt-10 pb-20 sm:ps-20% md:ps-64">
         <p className="font-mono font-black text-8xl sm:text-9xl md:text-9xl tracking-wide dark:text-white">
           I<span className="text-lime-400">&#39;</span>M <br />
@@ -14,6 +15,9 @@ export default function Home() {
           Front End Web developer <span>&#x26;</span> <br />
           Freelancer
         </p>
+      </div>
+      <div className="dark:bg-slate-600/75 pb-10 relative z-10">
+        <Intro className="md:w-3/4 mx-auto dark:bg-slate-600 flex flex-col items-center justify-center p-4 gap-3 md:gap-5 md:shadow-black/50 md:shadow-md"/>
       </div>
       <div className="absolute w-full h-full dark:bg-slate-600/25 left-0 right-0 top-0 bottom-0 z-0"></div>
     </div>
