430cd8628a26a1d6d1ce03741fe7671f8fbb1465
Hamid Azim <hamidazim321@gmail.com>
2023-12-17 20:43:49 +0500

    Add skillbars

---
+++ src/components/SkillBar.jsx
@@ -0,0 +1,12 @@
+export default function SkillBar({tech, level, className, barStyles}) {
+  const textStyles = 'font-serif text-xl block dark:text-white w-auto sm:text-2xl md:text-2xl'
+  return (
+    <div className={className}>
+      <span className={`${textStyles} dark:text-white/50`}>{tech}</span>
+      <div className="flex-grow h-auto bg-black/25">
+        <span className={`${barStyles} block`} style={{ width: `${level}%` }}></span>
+      </div>
+      <span className={textStyles}>{level}%</span>
+    </div>
+  )
+}
--- src/pages/Professional.jsx
+++ src/pages/Professional.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Overlay from "../components/Overlay";
+import SkillBar from "../components/SkillBar";
 
 export default function Professional() {
+  const skillBarStyles = "flex items-center w-full gap-2"
   return (
-    <div className="relative bg-black/50">
+    <div className="relative dark:bg-black/50 p-4">
       <div className="relative z-10 p-4 flex flex-col items-center justify-center gap-3">
         <p className="dark:text-white font-mono font-black text-3xl sm:text-4xl md:text-5xl">
           <span className="me-2 dark:text-white/50 text-black/50">01</span>Professional
@@ -11,6 +13,38 @@ export default function Professional() {
         <p className="font-sans font-normal dark:text-white text-xl sm:text-2xl md:text-2xl">
           My Knowledge level in tech stacks
         </p>
+      </div>
+      <div className="relative z-10 flex flex-col gap-3 md:gap-5">
+        <SkillBar 
+        className={skillBarStyles}
+        tech={'React'} 
+        level={'80'}
+        barStyles={`h-2 bg-green-500`}
+        />
+        <SkillBar 
+        className={skillBarStyles}
+        tech={'JavaScript'} 
+        level={'95'}
+        barStyles={`h-2 bg-green-500`}
+        />
+        <SkillBar 
+        className={skillBarStyles}
+        tech={'CSS3'} 
+        level={'80'}
+        barStyles={`h-2 bg-green-500`}
+        />
+        <SkillBar 
+        className={skillBarStyles}
+        tech={'Python'} 
+        level={'70'}
+        barStyles={`h-2 bg-green-500`}
+        />
+        <SkillBar 
+        className={skillBarStyles}
+        tech={'Ruby'} 
+        level={'70'}
+        barStyles={`h-2 bg-green-500`}
+        />
       </div>
       <Overlay />
     </div>
