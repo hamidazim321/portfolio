317a7361e597ddab9a2b2e6f00cb08da5d2ad69c
Hamid Azim <hamidazim321@gmail.com>
2023-12-18 14:48:08 +0500

    Create ProjectCard component

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SectionHeading from "../components/SectionHeading";
-import image from "../assets/Headshot.png";
-import image2 from "../assets/hardware.jpg";
+import ProjectCard from "../components/ProjectCard";
 export default function Portfolio() {
   return (
     <div className="dark:bg-slate-700">
@@ -11,6 +10,7 @@ export default function Portfolio() {
         desc={"My Latest Work"}
       />
       <div className="Cards here">
+        <ProjectCard img={'../assets/hardware.jpg'}/>
       </div>
     </div>
   );
