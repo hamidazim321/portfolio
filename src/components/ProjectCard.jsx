4713cc107e6ecd90e4188a9cab42616a5b6da2a0
Hamid Azim <hamidazim321@gmail.com>
2023-12-18 17:09:18 +0500

    Change structure of Project Card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,26 +6,40 @@ export default function ProjectCard({img}) {
       <div className="group md:flex bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="md:w-1/2 md:group-odd:order-2">
           <img
-            className="rounded-lg md:aspect-square"
+            className="rounded-lg"
             src={image}
             alt=""
           />
         </div>
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
-            </h5>
-          </a>
+        <div className="p-5 flex-grow">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Project Title
+          </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+            Project Description
           </p>
+          <ul className="flex gap-3">
+            <li>
+              Tech Stack
+            </li>
+            <li>
+              Tech Stack
+            </li>
+            <li>
+              Tech Stacks
+            </li>
+          </ul>
           <a
             href="#"
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Read more
+            Link 1
+          </a>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Link 2
           </a>
         </div>
       </div>
