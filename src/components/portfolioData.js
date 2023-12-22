471cf5001175af39dd6f97424be2c46cd211e658
Hamid Azim <hamidazim321@gmail.com>
2023-12-22 19:00:22 +0500

    Compress all images in jpg

--- src/components/portfolioData.js
+++ src/components/portfolioData.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     name: "Klink",
-    img: "klink.png",
+    img: "klink.jpg",
     stacks: ["React.js", "Redux", "JavaScript", "Bootstrap", "Firebase"],
     desc: `Klink is a full-stack text-based social media web app backed by 
     firebase backend services. Main features of Klink is user authentication, 
@@ -12,7 +12,7 @@ const projects = [
 
   {
     name: "Coin Watch",
-    img: "coinWatch.png",
+    img: "coinWatch.jpg",
     stacks: ["react", "redux", "JavaScript"],
     desc: `coin Watch is an SAP (Single Page Application) created using React.js 
     for the crypto enthusiast to get accurate updates on crypto prices and more`,
@@ -22,7 +22,7 @@ const projects = [
 
   {
     name: "Vintage Car Show",
-    img: "CarShow.png",
+    img: "CarShow.jpg",
     stacks: ["HTML5", "CSS3", "JavaScript"],
     desc: `Vintage Car Show is a Website for a Fictional Vintage Car Show in Miami. 
     It built on the MPA (Multi-Page Application) Model. This was a capstone project for the
@@ -33,7 +33,7 @@ const projects = [
 
   {
     name: "My Meal",
-    img: "MyMeal.png",
+    img: "MyMeal.jpg",
     stacks: ["HTML5", "CSS3", "JavaScript"],
     desc: `My Meal is an SAP (Single Page Application) that uses several data from APIs 
     to display different meals according to user preferences such as Country, Category, 
