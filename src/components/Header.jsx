2b38f3ed767d7fc9f3ccc3fbb2b394e8e9d477b8
Hamid Azim <hamidazim321@gmail.com>
2023-12-22 15:45:36 +0500

    Fix bg on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,7 +81,7 @@ export default function Header() {
           <ul
             className={`${
               menuOn && "absolute w-full"
-            } md:static font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700`}
+            } md:static font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700`}
           >
             <li className="md:dark:hover:bg-slate-900/50 rounded">
               <a
