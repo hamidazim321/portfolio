3f2c194456e546387a88313edb67c350cc856274
Hamid Azim <hamidazim321@gmail.com>
2023-12-22 18:14:34 +0500

    Handle form loading

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,21 @@
-import React, { useEffect } from "react";
 import { FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa6";
 import { sendEmail } from "../email";
+import { useState } from "react";
 
 export default function Contact() {
+  const [emailResp, setEmailResp] = useState(null);
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    e.target.reset()
+    setLoading(true)
+    const response = await sendEmail(e.target);
+    await setEmailResp(response);
+    await setLoading(false)
+  };
   return (
-    <section
-      className="bg-laptop h-full relative bg-no-repeat bg-cover md:bg-cover bg-center bg-fixed dark:bg-slate-600"
-    >
+    <section className="bg-laptop h-full relative bg-no-repeat bg-cover md:bg-cover bg-center bg-fixed dark:bg-slate-600">
       <div className="dark:bg-slate-600/75 bg-slate-100/75 relative z-10 py-20">
         <div
           id="contact"
@@ -23,7 +32,10 @@ export default function Contact() {
               </p>
             </div>
             <div className="flex flex-col w-full md:w-1/2">
-              <form onSubmit={sendEmail} className="flex flex-col gap-3">
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col gap-3"
+              >
                 <div className="flex flex-col md:flex-row gap-3">
                   <div>
                     <label
@@ -69,15 +81,34 @@ export default function Contact() {
                     rows="4"
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
                     placeholder="Write your thoughts here..."
+                    required
                   ></textarea>
                 </div>
-                <div>
-                  <button className="relative w-full inline-flex items-center justify-center  overflow-hidden text-sm font-medium text-gray-900 group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                    <span className="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent md:bg-white dark:bg-gray-900 md:group-hover:bg-opacity-0">
-                      Send
-                    </span>
-                  </button>
-                </div>
+                {emailResp &&
+                  (emailResp == 200 ? (
+                    <div
+                      className="p-4 text-center mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                      role="alert"
+                    >
+                      <span className="font-medium">Success!</span> Your message has been received
+                    </div>
+                  ) : (
+                    <div
+                      class="p-4 text-center mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+                      role="alert"
+                    >
+                      <span class="font-medium">Fail!</span> Your message could not be received.
+                    </div>
+                  ))}
+                <button
+                  type="submit"
+                  className="relative w-full inline-flex items-center justify-center  overflow-hidden text-sm font-medium text-gray-900 group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+                  disabled = {loading}
+                >
+                  <span className="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent md:bg-white dark:bg-gray-900 md:group-hover:bg-opacity-0">
+                    Send
+                  </span>
+                </button>
               </form>
             </div>
           </div>
