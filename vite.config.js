3f4e5442d59e9025a4455fd266ab8b2146e2ee5f
Hamid Azim <hamidazim321@gmail.com>
2023-12-30 16:35:23 +0500

    Update vite configs

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    },
-    rewrites: [
-      {
-        source: '**',
-        destination: '/index.html'
-      }
-    ]
-  }
 })
