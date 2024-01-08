5555d158fb9f3fe1e57ac2690f8c22f315fe5910
Hamid Azim <hamidazim321@gmail.com>
2024-01-08 18:54:51 +0500

    Add certificate

--- src/components/CertificatesGallery.jsx
+++ src/components/CertificatesGallery.jsx
@@ -1,59 +1,89 @@
 import React from "react";
 import img1 from "../assets/certificates/web-development-ibm.webp";
-import img2 from "../assets/certificates/javascript-microverse.webp";
-import img3 from "../assets/certificates/html-microverse.webp";
-import img4 from "../assets/certificates/react-redux.webp";
-import img5 from "../assets/certificates/python-coursera.webp";
-import img6 from "../assets/certificates/javascript-meta.webp";
+import img2 from "../assets/certificates/python-coursera.webp";
+import img3 from "../assets/certificates/javascript-meta.webp";
+import img4 from "../assets/certificates/javascript-microverse.webp";
+import img5 from "../assets/certificates/html-microverse.webp";
+import img6 from "../assets/certificates/react-microverse.webp";
+import img7 from "../assets/certificates/ruby-microverse.webp"
 export default function CertificatesGallery() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-center justify-center">
-        <a
-          href="https://www.credly.com/badges/2369908a-f82d-49ac-b714-1134d0511986/public_url"
-          target="_blank"
-        >
-          <img src={img1} className="h-auto max-w-full rounded-lg" />
-        </a>
-      </div>
-      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-center justify-center">
-        <a
-          href="https://www.credential.net/f54e979c-c28a-4df3-8dcb-f0ec03d229a4"
-          target="_blank"
-        >
-          <img src={img2} className="h-auto max-w-full rounded-lg" />
-        </a>
-      </div>
-      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-center justify-center">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-end justify-center">
         <a
           href="https://www.credential.net/5d44d341-f805-448a-af6c-ddb93a1a0321"
           target="_blank"
         >
-          <img src={img3} className="h-auto max-w-full rounded-lg" />
+          <figure>
+            <img src={img1} className="h-auto max-w-full rounded-lg" alt="Web Development Essentials"/>
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Web Development Essentials</figcaption>
+          </figure>
+        </a>
+      </div>
+      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-end justify-center">
+        <a
+          href="https://www.credential.net/5d44d341-f805-448a-af6c-ddb93a1a0321"
+          target="_blank"
+        >
+          <figure>
+            <img src={img2} className="h-auto max-w-full rounded-lg" alt="Python For Everybody Specialization" />
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Python For Everybody Specialization</figcaption>
+          </figure>
+        </a>
+      </div>
+      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-end justify-center">
+        <a
+          href="https://www.credential.net/5d44d341-f805-448a-af6c-ddb93a1a0321"
+          target="_blank"
+        >
+          <figure>
+            <img src={img3} className="h-auto max-w-full rounded-lg" alt="Programming with JavaScript" />
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Programming with JavaScript</figcaption>
+          </figure>
+        </a>
+      </div>
+      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-end justify-center">
+        <a
+          href="https://www.credential.net/5d44d341-f805-448a-af6c-ddb93a1a0321"
+          target="_blank"
+        >
+          <figure>
+            <img src={img4} className="h-auto max-w-full rounded-lg" alt="Microverse JavaScript Module" />
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Microverse JavaScript Module</figcaption>
+          </figure>
+        </a>
+      </div>
+      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-end justify-center">
+        <a
+          href="https://www.credential.net/5d44d341-f805-448a-af6c-ddb93a1a0321"
+          target="_blank"
+        >
+          <figure>
+            <img src={img5} className="h-auto max-w-full rounded-lg" alt="Microverse HTML/CSS Module" />
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Microverse HTML/CSS Module</figcaption>
+          </figure>
+        </a>
+      </div>
+      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-end justify-center">
+        <a
+          href="https://www.credential.net/5d44d341-f805-448a-af6c-ddb93a1a0321"
+          target="_blank"
+        >
+          <figure>
+            <img src={img6} className="h-auto max-w-full rounded-lg" alt="Microverse React/Redux Module" />
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Microverse React/Redux Module</figcaption>
+          </figure>
         </a>
       </div>
       <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-center justify-center">
         <a
-          href="https://www.credential.net/0c993c52-4477-4d35-8329-5f80509841a6"
+          href="https://www.credential.net/630658f1-c29c-4935-826e-b004c237b57f"
           target="_blank"
         >
-          <img src={img4} className="h-auto max-w-full rounded-lg" />
-        </a>
-      </div>
-      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-center justify-center">
-        <a
-          href="https://coursera.org/share/01f81d18edbed6a901a7385de8bf2e4f"
-          target="_blank"
-        >
-          <img src={img5} className="h-auto max-w-full rounded-lg" />
-        </a>
-      </div>
-      <div className="p-2 bg-gray-300 dark:bg-gray-800 rounded-lg flex items-center justify-center">
-        <a
-          href="https://coursera.org/share/b0c0d385f138b5ecba221cd74f2b459c"
-          target="_blank"
-        >
-          <img src={img6} className="h-auto max-w-full rounded-lg" />
+          <figure>
+            <img src={img7} className="h-auto max-w-full rounded-lg" alt="Microverse Ruby/Databases Module" />
+            <figcaption className="text-center dark:text-white text-gray-700 font-bold">Microverse Ruby/Databases Module</figcaption>
+          </figure>
         </a>
       </div>
     </div>
--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -4,8 +4,8 @@ import SectionHeading from "../components/SectionHeading";
 
 export default function Certificates() {
   return (
-    <section className="py-24 px-4 bg-gray-100 dark:bg-gray-900">
-      <SectionHeading headingIndex={"03"} heading={"My Credientials"} />
+    <section className="pt-24 pb-4 px-4 bg-gray-100 dark:bg-gray-900">
+      <SectionHeading headingIndex={"03"} heading={"Credientials"} />
       <CertificatesGallery />
     </section>
   );
